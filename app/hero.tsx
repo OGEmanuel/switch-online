5bdd8958fa0155feaedf676523cf06a26e94a38d
OGEmanuel <1blacksanta416@gmail.com>
2024-04-09 18:01:23 +0100

    Testing yt

--- app/hero.tsx
+++ app/hero.tsx
@@ -93,21 +93,22 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="absolute top-[16rem] right-[18rem] xl:block hidden">
+      <div className="absolute top-[10rem] right-[2rem] xl:block hidden">
         <div className="relative">
           <Image
-            src={tao}
+            src={yt}
             alt="first"
-            width={200}
-            height={200}
+            width={190}
+            height={113}
             className="rounded-2xl"
             unoptimized
           />
-          <div className="absolute top-[90%] left-[50%] translate-x-[-50%] translate-y-[-20%]">
-            <HeroXIcon />
+          <div className="absolute top-[90%] left-[50%] translate-x-[-50%] translate-y-[-30%]">
+            <HeroYtIcon />
           </div>
         </div>
       </div>
+
       {/* <div className="absolute -top-[5.5rem] right-[12rem] xl:block hidden">
         <div className="relative">
           <Image
