6f4a4655822e539f530531155992e858f0940211
OGEmanuel <1blacksanta416@gmail.com>
2024-04-10 07:24:52 +0100

    Another openGraph attempt

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,31 @@ import "./globals.css";
 import Navbar, { NavLink } from "@/components/navbar";
 import Footer from "@/components/footer";
 import Banner from "@/components/banner";
-import Head from "next/head";
+import ogImage from "@/public/switch-online-home.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Switch Online",
   description: "Unlock Your Brand's Potential with Switch.",
+  openGraph: {
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -19,35 +37,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <Head>
-        <meta
-          property="og:image"
-          content="https://i.ibb.co/9wZqKtw/Switch-Online-Home.png"
-        />
-        <meta property="og:title" content="Switch Online"></meta>
-        <meta
-          property="og:description"
-          content="Unlock Your Brand's Potential with Switch."
-        />
-      </Head>
-      <html lang="en">
-        <body className={inter.className}>
-          <Navbar>
-            <NavLink href={"/"}>Home</NavLink>
-            <NavLink href={"/our-services"}>Our services</NavLink>
-            <NavLink href={"/case-studies"}>Case studies</NavLink>
-            <NavLink href={"/about-us"}>About us</NavLink>
-          </Navbar>
-          {children}
-          <Banner />
-          <Footer>
-            <NavLink href={"/our-services"}>Our services</NavLink>
-            <NavLink href={"/case-studies"}>Case studies</NavLink>
-            <NavLink href={"/about-us"}>About us</NavLink>
-          </Footer>
-        </body>
-      </html>
-    </>
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar>
+          <NavLink href={"/"}>Home</NavLink>
+          <NavLink href={"/our-services"}>Our services</NavLink>
+          <NavLink href={"/case-studies"}>Case studies</NavLink>
+          <NavLink href={"/about-us"}>About us</NavLink>
+        </Navbar>
+        {children}
+        <Banner />
+        <Footer>
+          <NavLink href={"/our-services"}>Our services</NavLink>
+          <NavLink href={"/case-studies"}>Case studies</NavLink>
+          <NavLink href={"/about-us"}>About us</NavLink>
+        </Footer>
+      </body>
+    </html>
   );
 }
