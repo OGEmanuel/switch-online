9290f17de453666976b4b55b1ea05e42ea910576
OGEmanuel <1blacksanta416@gmail.com>
2024-04-28 00:52:16 +0100

    Minor ui fixes

--- components/feature-box.tsx
+++ components/feature-box.tsx
@@ -25,37 +25,31 @@ const FeatureBox = ({
 
   return (
     <div
-      className={`lg:pt-[7.5rem] pt-12 overflow-hidden w-full px-4 md:px-10 lg:px-20 ${
-        theme === "dark" ? "bg-[#020F0F]" : "bg-white"
-      }`}
+      className={`lg:pt-[7.5rem] pt-12 overflow-hidden w-full px-4 md:px-10 lg:px-20 flex flex-col items-center justify-between gap-4 lg:gap-16 ${classes} mx-auto max-w-[1440px]`}
     >
-      <div
-        className={`flex flex-col items-center justify-between gap-4 lg:gap-16 ${classes} mx-auto max-w-[1440px]`}
-      >
-        <div className="lg:flex-shrink-0">
-          <DisplayWithBolt display={display} />
-        </div>
-        <div className="flex-grow">
-          <h2
-            className={`md:text-[2rem] lg:text-[1.75rem] text-[1.75rem] xl:text-[2.5rem] font-extrabold xl:leading-[3rem] mb-3 2xl:mb-6 ${
-              theme === "dark" ? "text-white" : ""
-            }`}
-          >
-            {title}
-          </h2>
-          <p
-            className={`text-base 2xl:text-lg 2xl:leading-[1.8rem] mb-4 2xl:mb-8 ${
-              theme === "dark" ? "text-[#BAD0D0]" : "text-[#506363]"
-            }`}
-          >
-            {children}
-          </p>
-          <CtaRightArrow
-            href={href}
-            width="w-[12rem] md:w-[15.125rem]"
-            cta={cta}
-          />
-        </div>
+      <div className="lg:flex-shrink-0">
+        <DisplayWithBolt display={display} />
+      </div>
+      <div className="flex-grow">
+        <h2
+          className={`md:text-[2rem] lg:text-[1.75rem] text-[1.75rem] xl:text-[2.5rem] font-extrabold xl:leading-[3rem] mb-3 2xl:mb-6 ${
+            theme === "dark" ? "text-white" : ""
+          }`}
+        >
+          {title}
+        </h2>
+        <p
+          className={`text-base 2xl:text-lg 2xl:leading-[1.8rem] mb-4 2xl:mb-8 ${
+            theme === "dark" ? "text-[#BAD0D0]" : "text-[#506363]"
+          }`}
+        >
+          {children}
+        </p>
+        <CtaRightArrow
+          href={href}
+          width="w-[12rem] md:w-[15.125rem]"
+          cta={cta}
+        />
       </div>
     </div>
   );
